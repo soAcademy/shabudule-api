783ae7f1f3bd5341ffa05def1c3110762e78aa95
BornDhamachart <dhamachart.c@gmail.com>
2023-03-29 15:56:01 +0700

    feat:Update API with authentication

--- src/shabudule API/shabudule.resolver.ts
+++ src/shabudule API/shabudule.resolver.ts
@@ -38,7 +38,7 @@ const serciveAccountKey = {
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
   private_key: process.env.PRIVATE_KEY
-    ? process.env.PRIVATE_KEY.replace(/\\n/gm, "\n")
+    ? process.env.PRIVATE_KEY.replace(/\\n/g, "\n")
     : undefined,
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
